354d6c39817084ed528a489e9d84b685e7357f10
Fabby24 <musaufabian7@gmail.com>
2024-05-24 11:15:27 +0300

    feature sign up working

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter your current ttlocation: ")
+print(f"Hello {name}, you are {age} years old and you live in {location}") 
